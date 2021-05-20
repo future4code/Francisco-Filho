422fb043910c709300b32d4c343ef2a375709325
falcaogodoy <falcaogodoy@gmail.com>
2021-05-20 17:48:16 -0300

    exercicio variaveis

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,68 @@
+/*  ex 1
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b) 
+
+ Mostra no console os numeros 10 e 5 */
+
+/* ex 2
+let a = 10
+let b = 20
+let c
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+Mostra no console o numero 10 */
+
+/* ex 3
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+Os melhores nomes para as variaves seriam variavel p = horasTrabalhadas e variavel t = salarioDiario */
+
+
+/* Exercícios de escrita de código */
+/* exercicio 01 */
+//const nome = prompt(" Qual seu nome?")
+//const idade = prompt("Qual sua Idade?")
+
+//const idadeConvertida = Number(idade)
+//console.log("Ola ", nome,"voce tem ". idadeConvertida, "anos") 
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof idadeConvertida)
+/* o Tipo impresso foi String, pois eu não especifiquei qual a entrada da variavel. */
+
+/* Exercícios de escrita de código */
+/* exercicio 02 */
+
+//const pergunta01 = prompt("Você está usando uma roupa azul hoje?")
+//const pergunta02 = prompt("Voce bebeu agua hoje?")
+//const pergunta03 = prompt("Você comeu hoje ?")
+
+//console.log("Você está usando uma roupa azul hoje? - ", pergunta01)
+//console.log("Voce bebeu agua hoje? - ", pergunta02)
+//console.log("Voce comeu hoje ? - ", pergunta03)
+
+/* Exercícios de escrita de código */
+/* exercicio 03 */
+
+let a = 10
+let b = 25
+let c = a
+
+// Aqui faremos uma lógica para trocar os valores
+
+a = b
+b = c
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
