0c124341f7a11bd4cf77f96f6aca36cbfddb7e8e
falcaogodoy <falcaogodoy@gmail.com>
2021-07-15 11:58:07 -0300

    curriculo

--- quinzena5/aula17/labetube-react/src/App.js
+++ quinzena5/aula17/labetube-react/src/App.js
@@ -19,8 +19,8 @@ function App() {
             <nav className="menu-vertical">
                 <ul>
                     <li className="botoes-meunu-vertical">Início</li>
-                    <li classNames="botoes-meunu-vertical">Em alta</li>
-                    <li classNames="botoes-meunu-vertical">Inscrições</li>
+                    <li className="botoes-meunu-vertical">Em alta</li>
+                    <li className="botoes-meunu-vertical">Inscrições</li>
                     <hr/>
                     <li className="botoes-meunu-vertical">Originais</li>
                     <li className="botoes-meunu-vertical">Histórico</li>
