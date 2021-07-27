81e2deea01a70e661c14d90e9b96583b0c8db529
falcaogodoy <falcaogodoy@gmail.com>
2021-07-27 18:22:30 -0300

    formulario

---
+++ semana7/aula21/formulario/src/App.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+import Etapa1 from './pages/Etapa1';
+import Etapa2 from './pages/Etapa2';
+import Etapa3 from './pages/Etapa3';
+import Final from './pages/Final';
+import React from 'react';
+
+
+
+
+class App extends React.Component {
+state = {
+  etapa: 1, 
+}  
+
+renderizaEtapa = () => {
+  switch (this.state.etapa) {
+    case 1: 
+      return <Etapa1/>;
+      break;
+
+    case 2: 
+      return <Etapa2/>;
+      break;
+
+    case 3: 
+    return <Etapa3/>;
+    break;
+    case 4: 
+    return <Final/>;
+    break;
+    default: return <Etapa1/>
+    break;
+
+}
+}
+irParaProximaEtapa = () =>{
+   
+  this.setState({etapa: this.state.etapa + 1});
+}
+
+  render () {
+    return (
+      <div>
+        {this.renderizaEtapa()}
+        <button onClick={this.irParaProximaEtapa}>Próxima Etapa</button>
+      </div>
+    );
+    }
+  }
+
+
+
+export default App;
---
+++ semana7/aula21/formulario/src/pages/Final.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+
+function Final () {
+    return (
+      <div className="Final">
+          <h1>Obrigado Pelas Respostas! </h1>
+        
+        
+      </div>
+    );
+  }
+  
+  export default Final;
